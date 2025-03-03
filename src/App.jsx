ee606102860bd4bb484079e10041c09a24746fda
Gerard Eklu <gerard.eklu@yahoo.fr>
2025-03-02 21:38:45 -0500

    Implemented the trending movies logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
-import { updateSearchCount } from './appwrite';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -22,6 +22,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
   
@@ -66,9 +67,25 @@ const App = () => {
     }
   }
 
+  const loadTrendingMovies = async() => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } 
+    catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm)
-  }, [debouncedSearchTerm])
+  }, [debouncedSearchTerm]);
+
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, [])
+
 
 
   return (
@@ -82,8 +99,28 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
+        {
+          trendingMovies.length > 0 && (
+            <section className='trending'>
+              <h2>Trending Movies</h2>
+
+              <ul>
+                {
+                  trendingMovies.map((movie, index) => (
+                    <li key={movie.$id}>
+                      <p>{index + 1}</p>
+                      <img src={movie.poster_url} alt={movie.title}/>
+                    </li>
+                  ))
+                }
+              </ul>
+
+            </section>
+          )
+        }
+
         <section className='all-movies'>
-          <h2 className='mt-[40px]'>All Movies</h2>
+          <h2>All Movies</h2>
 
           {
             isLoading ? (
@@ -95,15 +132,12 @@ const App = () => {
               {movieList.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
               ))}
-            </ul>
-              
+            </ul>            
           )
           }
         </section>
-
       </div>
-    </main>
-   
+    </main>  
   )
 }
 
